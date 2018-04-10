9f11faef7301066cd7610d2cc2c201255b8c11ab
rola45 <28305960+rola45@users.noreply.github.com>
2018-04-09 23:44:58 -0700

    Add files via upload

---
+++ templates/footer.php
@@ -0,0 +1,11 @@
+<div class="clearfix"></div>
+	<!-- Footer -->
+    <footer>
+    	<div class="container-fluid">
+    		<p class="text-center">LOS INFORMATICOS FCAYS</a> <?php echo date("Y")?></p>
+    	</div>
+    </footer>
+    <!-- /Footer -->
+  </body>
+</html>
+<?php ob_end_flush(); ?>
